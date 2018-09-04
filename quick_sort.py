a116e02ea852450e478c5271a7468032377395cb
facaj <facajx@gmail.com>
2018-09-04 09:10:43 -0300

    Add files via upload

---
+++ quick_sort.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#Aluno: Francisco de Assis
+
+import time
+from optparse import OptionParser
+
+
+def Partition(array,p,fim):
+	x = array[p]
+	a = p+1
+	b = fim
+	while(True):
+		time.sleep(1)
+		print("B->"+str(b)+" A->"+str(a))
+		while array[b]>x and b>=a:
+			b=b-1
+		while array[a]<x and a<=b:
+			a=a+1
+		if a<b:
+			array[a],array[b]=array[b],array[a]
+			#print("loop")
+		else:
+			array[p],array[b]=array[b],array[p]
+			return b
+
+def QuickSort(array,inicio,fim):
+	if inicio < fim:
+		q= Partition(array,inicio,fim)
+		print (q)
+		QuickSort(array,inicio,q-1)
+		QuickSort(array,q+1,fim)
+	return array
+
+argv = OptionParser()
+argv.add_option("-i", "--instancias", action = "store", dest = "arquivo",default="",help = "Arquivo com valores para ordenar")
+(options,args) = argv.parse_args()
+
+if options.arquivo != "":
+	a=[]
+	try:
+		arq=open(options.arquivo,'r')
+		a=arq.read().splitlines()
+		arq.close()
+	except:
+		print('Erro ao ler o arquivo')
+
+	inicio = time.time()
+	a=[3,5,62,6,7,4,3,7,5,8]
+	print(a)
+	print (QuickSort(a,0,int(len(a))-1))
+	print ('Tempo gasto: ' + str(time.time() - inicio) + ' segundos')
+else:
+	argv.error("Passe o arquivo com instância")
